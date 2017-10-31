4a7d0e13f877cd940ce12e2a04ed74c9ca1baa98
Sunil  K S <sunil.ks29@gmail.com>
2017-10-31 08:41:35 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,19 @@
 class Post
-  attr_accessor :title,:author
-  def initialize(name)
-    @title = name
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
   end
+
+  def has_author?
+    !!self.author
+  end
+
   def author_name
-    if !!@author
-      @author.name
+    if has_author?
+      self.author.name
     else
       nil
     end
+  end
 end
